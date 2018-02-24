d65cba366d40f1a0be3324248280daa3ad9cc055
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-02-24 10:24:27 +0900

    update AndroidManifest.xml

--- XFCameraMediaPluginSample/XFCameraMediaPluginSample/MainPage.xaml.cs
+++ XFCameraMediaPluginSample/XFCameraMediaPluginSample/MainPage.xaml.cs
@@ -7,11 +7,42 @@ using Xamarin.Forms;
 
 namespace XFCameraMediaPluginSample
 {
-	public partial class MainPage : ContentPage
+    using Plugin.Media;
+
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+
+
+		    this.btnTakePhoto.Clicked += async (sender, args) =>
+		        {
+		            await CrossMedia.Current.Initialize();
+
+		            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+		            {
+		                DisplayAlert("No Camera", ":( No camera available.", "OK");
+		                return;
+		            }
+
+		            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+		                                                                   {
+		                                                                       Directory = "Sample",
+		                                                                       Name = "test.jpg"
+		                                                                   });
+
+		            if (file == null)
+		                return;
+
+		            await DisplayAlert("File Location", file.Path, "OK");
+
+		            imagePhoto.Source = ImageSource.FromStream(() =>
+		                {
+		                    var stream = file.GetStream();
+		                    return stream;
+		                });
+		        };
 		}
 	}
 }
