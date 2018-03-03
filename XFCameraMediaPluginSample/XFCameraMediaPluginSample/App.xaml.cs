5c2b13a8b66fde0d4ea893d3ca527fae5412f95f
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-03-03 18:59:52 +0900

    update to Prism and add ActivityIndicator

--- XFCameraMediaPluginSample/XFCameraMediaPluginSample/App.xaml.cs
+++ XFCameraMediaPluginSample/XFCameraMediaPluginSample/App.xaml.cs
@@ -1,34 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using Prism;
+using Prism.Ioc;
+using XFCameraMediaPluginSample.ViewModels;
+using XFCameraMediaPluginSample.Views;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Prism.Unity;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace XFCameraMediaPluginSample
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : PrismApplication
+    {
+        /* 
+         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+         * This imposes a limitation in which the App class must have a default constructor. 
+         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+         */
+        public App() : this(null) { }
 
-			MainPage = new XFCameraMediaPluginSample.MainPage();
-		}
+        public App(IPlatformInitializer initializer) : base(initializer) { }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage>();
+        }
+    }
 }
--- XFCameraMediaPluginSample/XFCameraMediaPluginSample/ViewModels/MainPageViewModel.cs
+++ XFCameraMediaPluginSample/XFCameraMediaPluginSample/ViewModels/MainPageViewModel.cs
@@ -1,18 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Plugin.Media;
+using Prism.Services;
+using Xamarin.Forms;
 
 namespace XFCameraMediaPluginSample.ViewModels
 {
-    class MainPageViewModel : BindableBase
+    public class MainPageViewModel : BindableBase, INavigationAware
     {
+        private INavigationService NavigationService;
+
+        private IPageDialogService PageDialogService { get; }
+
+        private ImageSource imageSource;
+
+        public ImageSource ImageSource
+        {
+            get { return this.imageSource; }
+            set { this.SetProperty(ref this.imageSource, value); }
+        }
+
+        public DelegateCommand TakePhotoCommand { get; }
+
         private bool isBusy;
 
         public bool IsBusy
         {
             get { return this.isBusy; }
             set { this.SetProperty(ref this.isBusy, value); }
+        }
+
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService)
+        {
+            NavigationService = navigationService;
+            this.PageDialogService = pageDialogService;
+            this.TakePhotoCommand = new DelegateCommand(async () => await this.TakePhotoAsync());
+        }
+
+        private async Task TakePhotoAsync()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await PageDialogService.DisplayAlertAsync("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            try
+            {
+                var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Sample",
+                    Name = "test.jpg"
+                });
+                IsBusy = true;
+
+                if (file == null)
+                    return;
+
+                // GPSで位置情報を取得
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50; // <- 1. 50mの精度に指定
+
+                var position = await locator.GetPositionAsync(timeout: new TimeSpan(0, 0, 0, 100));
+
+                var msg = $"File Location={file.Path}\n\nAltitude={position.Altitude}\nLongitude={position.Longitude}";
+                await PageDialogService.DisplayAlertAsync("Message", msg, "OK");
+
+                ImageSource = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStream();
+                    return stream;
+                });
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
         }
     }
 }
