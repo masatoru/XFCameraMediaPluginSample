db6f6570c60933eb00cfb2ac944905e763cbddbd
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-03-03 17:05:26 +0900

    add IsBusy property

--- XFCameraMediaPluginSample/XFCameraMediaPluginSample/ViewModels/MainPageViewModel.cs
+++ XFCameraMediaPluginSample/XFCameraMediaPluginSample/ViewModels/MainPageViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Prism.Mvvm;
 
 namespace XFCameraMediaPluginSample.ViewModels
 {
-    class MainPageViewModel
+    class MainPageViewModel : BindableBase
     {
+        private bool isBusy;
+
+        public bool IsBusy
+        {
+            get { return this.isBusy; }
+            set { this.SetProperty(ref this.isBusy, value); }
+        }
     }
 }
