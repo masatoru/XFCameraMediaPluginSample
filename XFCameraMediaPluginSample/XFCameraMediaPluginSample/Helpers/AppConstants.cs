0f17b5a2a5478467964ade4c623c42e2e99a8bf0
Satoru Fujimori <satoru.fujimori@gmail.com>
2018-06-15 12:24:00 +0900

    add AppContants

---
+++ XFCameraMediaPluginSample/XFCameraMediaPluginSample/Helpers/AppConstants.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace XFCameraMediaPluginSample.Helpers
+{
+    public static class AppConstants
+    {
+        // Put constants here that are not of a sensitive nature
+
+        public static string AppCenterStart
+        {
+            get
+            {
+                string startup = string.Empty;
+
+                if (Guid.TryParse(Secrets.AppCenter_iOS_Secret, out Guid iOSSecret))
+                {
+                    startup += $"ios={iOSSecret};";
+                }
+
+                if (Guid.TryParse(Secrets.AppCenter_Android_Secret, out Guid AndroidSecret))
+                {
+                    startup += $"android={AndroidSecret};";
+                }
+
+                return startup;
+            }
+        }
+    }
+}
